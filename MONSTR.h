f4d4b5528d49c27b00f9eddc7d82d0b15419d0b5
jd-13 <jackd8989@gmail.com>
2016-07-03 13:05:54 +0100

    Update comments

--- MONSTR.h
+++ MONSTR.h
@@ -1,12 +1,26 @@
 /*
-  ==============================================================================
-
-    MONSTR.h
-    Created: 21 Jan 2016 9:53:21pm
-    Author:  Jack Devlin
-
-  ==============================================================================
-*/
+ *	File:		MONSTR.h
+ *
+ *	Version:	1.0.0
+ *
+ *	Created:	21/01/2016
+ *
+ *	This file is part of MONSTR.
+ *
+ *  MONSTR is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MONSTR is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MONSTR.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef MONSTR_H_INCLUDED
 #define MONSTR_H_INCLUDED
