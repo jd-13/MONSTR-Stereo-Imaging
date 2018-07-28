a7f3530ad386edbdbf069aa4a197c467136cc3f7
jd-13 <jackd8989@gmail.com>
2018-07-28 18:27:35 +0100

    Fix header comments

--- MONSTRLookAndFeel.cpp
+++ MONSTRLookAndFeel.cpp
@@ -1,12 +1,26 @@
 /*
-  ==============================================================================
-
-    MONSTRLookAndFeel.cpp
-    Created: 1 Mar 2016 6:51:29pm
-    Author:  Jack Devlin
-
-  ==============================================================================
-*/
+ *    File:        MONSTRLookAndFeel.cpp
+ *
+ *    Version:    1.0.0
+ *
+ *    Created:    01/03/2016
+ *
+ *    This file is part of MONSTR.
+ *
+ *  MONSTR is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MONSTR is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MONSTR.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include "MONSTRLookAndFeel.h"
 
