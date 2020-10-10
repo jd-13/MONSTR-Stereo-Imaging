14a03b1b345702517fc80cf9435950029fd1f7e9
jd-13 <jackd8989@gmail.com>
2020-10-10 11:50:03 +0100

    Remove makefile debugging

--- Scripts/CIBuild.sh
+++ Scripts/CIBuild.sh
@@ -15,20 +15,14 @@ sed -i 's#../JUCE/modules#/home/JUCE/modules#g' $SCRIPT_DIR/../MONSTR.jucer
 # Set DSPFilters path
 sed -i 's#../WECore/DSPFilters#WECore/DSPFilters#g' $SCRIPT_DIR/../MONSTR.jucer
 
-cat $SCRIPT_DIR/../MONSTR.jucer
-
 # Generate the makefile
 $JUCE_PATH/Projucer --resave $SCRIPT_DIR/../MONSTR.jucer
-
-cat $SCRIPT_DIR/../Builds/LinuxMakefile/Makefile
 
 # Set include paths
 sed -i 's#-I$(HOME)/WEA/WECore/WECore#-I../../WECore/WECore#g' $SCRIPT_DIR/../Builds/LinuxMakefile/Makefile
 
 # TODO: this path gets set wrong, not sure why
 sed -i 's#-I$(HOME)/WWECore/DSPFilters/shared/DSPFilters/include#-I../../WECore/DSPFilters/shared/DSPFilters/include#g' $SCRIPT_DIR/../Builds/LinuxMakefile/Makefile
-
-cat $SCRIPT_DIR/../Builds/LinuxMakefile/Makefile
 
 # Run the build
 cd $SCRIPT_DIR/../Builds/LinuxMakefile
