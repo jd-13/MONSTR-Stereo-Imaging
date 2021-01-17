52dc959d88a25e58e9a0cec303a9a18ffc826609
jd-13 <jackd8989@gmail.com>
2021-01-17 12:17:17 +0000

    Correct JUCE URL in macOS script

--- Scripts/BuildMacOS.sh
+++ Scripts/BuildMacOS.sh
@@ -13,7 +13,7 @@ echo "=== Using WE-Core $(git log --pretty=format:'%h' -n 1) ==="
 cd -
 
 echo "=== Downloading JUCE ==="
-wget https://github.com/juce-framework/JUCE/releases/download/6.0.5/juce-6.0.7-osx.zip \
+wget https://github.com/juce-framework/JUCE/releases/download/6.0.7/juce-6.0.7-osx.zip \
      -P $SCRIPT_DIR/..
 unzip $SCRIPT_DIR/../juce-6.0.7-osx.zip
 
