0c2fdb72e49557bf9a85bfb9d32203cc4e0b8dbe
jd-13 <jackd8989@gmail.com>
2021-10-24 12:26:46 +0100

    Disable curl from module settings

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -18,7 +18,6 @@
 
 // (You can add your own code in this section, and the Projucer will not overwrite it)
 #define JUCE_VST3_CAN_REPLACE_VST2 0
-#define JUCE_USE_CURL 0
 
 // [END_USER_CODE_SECTION]
 
@@ -225,7 +224,7 @@
 #endif
 
 #ifndef    JUCE_USE_CURL
- //#define JUCE_USE_CURL 1
+ #define   JUCE_USE_CURL 0
 #endif
 
 #ifndef    JUCE_LOAD_CURL_SYMBOLS_LAZILY
